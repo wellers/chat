74be5b0755337b7d85586f3e11fd96607ff46426
Paul Welbourne <paul_welbourne@yahoo.co.uk>
2023-09-14 15:45:41 +0100

    Update generatepassword.sh to fix typo.

--- rabbitmq/generatepassword.sh
+++ rabbitmq/generatepassword.sh
@@ -3,5 +3,5 @@ PWD_HEX=$(echo -n $1 | xxd -p)
 SALT="908D C60A" 
 HEX="$SALT $PWD_HEX"
 SHA256=$(echo -n $HEX | xxd -r -p | sha256sum)
-# This is thw pwd to be inserted on your rabbit load_definitions file
+# This is the pwd to be inserted on your rabbit load_definitions file
 echo "908D C60A $SHA256" | xxd -r -p | base64
